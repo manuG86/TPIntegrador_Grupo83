e18c693949d31db6b8598da4cdc1245a51b516b2
Manu Galarza <manu.galarza@gmail.com>
2025-11-03 16:23:32 +0100

    Cambios en nombres de anio_edicion y clasificacion_dewey para que coincidan con la base de datos

--- src/tpi_libro_ficha_schema.sql
+++ src/tpi_libro_ficha_schema.sql
@@ -17,7 +17,7 @@ CREATE TABLE libro (
     titulo VARCHAR(150) NOT NULL,
     autor VARCHAR(120) NOT NULL,
     editorial VARCHAR(100),
-    anio_edicion INT
+    anioEdicion INT
 );
 
 -- Tabla FichaBibliografica (B)
@@ -25,7 +25,7 @@ CREATE TABLE ficha_bibliografica (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     eliminado BOOLEAN DEFAULT FALSE,
     isbn VARCHAR(17) UNIQUE NOT NULL,
-    clasificacion_dewey VARCHAR(20),
+    clasificacionDewey VARCHAR(20),
     estanteria VARCHAR(20),
     idioma VARCHAR(30),
     libro_id BIGINT UNIQUE, -- Relación 1 a 1 con libro
